74399a7ebe1af2a4587dd53a6fafe7ef0b1bed20
Djruizp <ruizd31100779@gmail.com>
2024-12-13 17:26:53 -0600

    Added theme corrections using databs, added example footer, added Prices section

--- index.js
+++ index.js
@@ -8,32 +8,43 @@ const body = document.body;
 var DarkTheme = true;
 
 toggleButton.addEventListener('click', () => {
-    if(DarkTheme){
-        body.classList.remove("bg-dark");
-        body.classList.add("bg-light");
+    // if(DarkTheme){
+    //     body.classList.remove("bg-dark");
+    //     body.classList.add("bg-light");
 
+    //     moonIcon.classList.remove("d-none");
+    //     sunIcon.classList.add("d-none");
+
+    //     themeText.forEach(Text => {
+    //         Text.classList.remove("text-light");
+    //         Text.classList.add("text-dark");
+    //     });
+
+    //     DarkTheme = false;
+    // }else{
+    //     body.classList.remove("bg-light");
+    //     body.classList.add("bg-dark");
+
+    //     moonIcon.classList.add("d-none");
+    //     sunIcon.classList.remove("d-none");
+
+    //     themeText.forEach(Text => {
+    //         Text.classList.remove("text-dark");
+    //         Text.classList.add("text-light");
+    //     });
+
+    //     DarkTheme = true;
+    // }  
+    if(DarkTheme){
+        document.documentElement.setAttribute("data-bs-theme", "light");
         moonIcon.classList.remove("d-none");
         sunIcon.classList.add("d-none");
-
-        themeText.forEach(Text => {
-            Text.classList.remove("text-light");
-            Text.classList.add("text-dark");
-        });
-
         DarkTheme = false;
     }else{
-        body.classList.remove("bg-light");
-        body.classList.add("bg-dark");
-
+        document.documentElement.setAttribute("data-bs-theme", "dark");
         moonIcon.classList.add("d-none");
         sunIcon.classList.remove("d-none");
-
-        themeText.forEach(Text => {
-            Text.classList.remove("text-dark");
-            Text.classList.add("text-light");
-        });
-
         DarkTheme = true;
-    }   
-    feather.replace();
+    };
+    
 });
