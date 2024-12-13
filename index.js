03ae268d9b1fe8c694b85bbfabd16a3ec28afef4
Djruizp <ruizd31100779@gmail.com>
2024-12-13 16:32:33 -0600

    Added dark and light theme button and functionalities, Added feather icons

---
+++ index.js
@@ -0,0 +1,39 @@
+const toggleButton = document.getElementById("themeButton");
+const moonIcon = document.getElementById("MoonIcon");
+const sunIcon = document.getElementById("SunIcon");
+
+const themeText = document.querySelectorAll(".ThemeText");
+
+const body = document.body;
+var DarkTheme = true;
+
+toggleButton.addEventListener('click', () => {
+    if(DarkTheme){
+        body.classList.remove("bg-dark");
+        body.classList.add("bg-light");
+
+        moonIcon.classList.remove("d-none");
+        sunIcon.classList.add("d-none");
+
+        themeText.forEach(Text => {
+            Text.classList.remove("text-light");
+            Text.classList.add("text-dark");
+        });
+
+        DarkTheme = false;
+    }else{
+        body.classList.remove("bg-light");
+        body.classList.add("bg-dark");
+
+        moonIcon.classList.add("d-none");
+        sunIcon.classList.remove("d-none");
+
+        themeText.forEach(Text => {
+            Text.classList.remove("text-dark");
+            Text.classList.add("text-light");
+        });
+
+        DarkTheme = true;
+    }   
+    feather.replace();
+});
