ba215057fcb6a0b9d6c920a763d3216989dc4882
Djruizp <ruizd31100779@gmail.com>
2024-12-15 23:35:38 -0600

    removed empty files, added contact button behavior

--- js/index.js
+++ js/index.js
@@ -3,36 +3,11 @@ const moonIcon = document.getElementById("MoonIcon");
 const sunIcon = document.getElementById("SunIcon");
 const themeColorButtons = document.querySelectorAll(".themeColorButton")
 
+const contactButton = document.getElementById("contactBtn")
+
 var DarkTheme = true;
 
-toggleButton.addEventListener('click', () => {
-    // if(DarkTheme){
-    //     body.classList.remove("bg-dark");
-    //     body.classList.add("bg-light");
-
-    //     moonIcon.classList.remove("d-none");
-    //     sunIcon.classList.add("d-none");
-
-    //     themeText.forEach(Text => {
-    //         Text.classList.remove("text-light");
-    //         Text.classList.add("text-dark");
-    //     });
-
-    //     DarkTheme = false;
-    // }else{
-    //     body.classList.remove("bg-light");
-    //     body.classList.add("bg-dark");
-
-    //     moonIcon.classList.add("d-none");
-    //     sunIcon.classList.remove("d-none");
-
-    //     themeText.forEach(Text => {
-    //         Text.classList.remove("text-dark");
-    //         Text.classList.add("text-light");
-    //     });
-
-    //     DarkTheme = true;
-    // }  
+toggleButton.addEventListener('click', () => { 
     if(DarkTheme){
         document.documentElement.setAttribute("data-bs-theme", "light");
         moonIcon.classList.remove("d-none");
@@ -58,3 +33,9 @@ toggleButton.addEventListener('click', () => {
     };
     
 });
+
+contactButton.addEventListener('click', () => {
+    const contactSection = document.getElementById("Contacto");
+
+    contactSection.scrollIntoView();
+})
