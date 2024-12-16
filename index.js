0318c4ae8d8018b67a496662537425fce1726f46
Djruizp <ruizd31100779@gmail.com>
2024-12-15 22:18:52 -0600

    Added contact view section

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const toggleButton = document.getElementById("themeButton");
 const moonIcon = document.getElementById("MoonIcon");
 const sunIcon = document.getElementById("SunIcon");
+const themeColorButtons = document.querySelectorAll(".themeColorButton")
 
-const body = document.body;
 var DarkTheme = true;
 
 toggleButton.addEventListener('click', () => {
@@ -37,11 +37,23 @@ toggleButton.addEventListener('click', () => {
         document.documentElement.setAttribute("data-bs-theme", "light");
         moonIcon.classList.remove("d-none");
         sunIcon.classList.add("d-none");
+
+        themeColorButtons.forEach(button => {
+            button.classList.add("btn-light");
+            button.classList.remove("btn-dark");
+        });
+
         DarkTheme = false;
     }else{
         document.documentElement.setAttribute("data-bs-theme", "dark");
         moonIcon.classList.add("d-none");
         sunIcon.classList.remove("d-none");
+
+        themeColorButtons.forEach(button => {
+            button.classList.add("btn-dark");
+            button.classList.remove("btn-light");
+        });
+
         DarkTheme = true;
     };
     
