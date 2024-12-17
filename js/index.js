c1fd72647cfaa7243c31911589c6e2f7bdb8a20f
Djruizp <ruizd31100779@gmail.com>
2024-12-16 22:35:37 -0600

    added start button, some corrections

--- js/index.js
+++ js/index.js
@@ -5,6 +5,7 @@ const themeColorButtons = document.querySelectorAll(".themeColorButton")
 
 const contactButton = document.getElementById("contactBtn")
 
+const startButton = document.getElementById("startButton")
 var DarkTheme = true;
 
 toggleButton.addEventListener('click', () => { 
@@ -38,4 +39,25 @@ contactButton.addEventListener('click', () => {
     const contactSection = document.getElementById("Contacto");
 
     contactSection.scrollIntoView();
-})
+});
+
+let isVisible = false;
+window.addEventListener("scroll", () => {
+    const scrollPosition = window.scrollY;
+
+    if (scrollPosition > 500 && !isVisible) {
+        // Aparecer elemento
+        startButton.classList.remove("fade-out");
+        startButton.classList.add("fade-in");
+        isVisible = true;
+      } 
+      else if (scrollPosition <= 500 && isVisible) {
+        // Desaparecer elemento
+        startButton.classList.remove("fade-in");
+        startButton.classList.add("fade-out");
+        isVisible = false;
+      }
+});
+startButton.addEventListener('click', () =>{
+    document.documentElement.scrollIntoView();
+});
