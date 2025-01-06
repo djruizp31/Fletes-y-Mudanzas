d5ca4e2a414af8ea971469e5a2ce500dc081bd08
Djruizp31 <ruizd31100779@gmail.com>
2025-01-05 23:08:29 -0600

    added final price step general corrections and identation corrections in html

--- js/PriceCalculator.js
+++ js/PriceCalculator.js
@@ -31,145 +31,153 @@ return haversine(coords1.lat, coords1.lng, coords2.lat, coords2.lng).toFixed(2);
 }
 
 
-(async () => {
+
     
-    const nextStepButton = document.getElementById("nextStepButton");
-    const backStepButton = document.getElementById("backStepButton");
-    // const calculateButton = document.getElementById("calculateButton");
+const nextStepButton = document.getElementById("nextStepButton");
+const backStepButton = document.getElementById("backStepButton");
 
-    const steps = document.querySelectorAll(".step")
+const steps = document.querySelectorAll(".step")
 
-    const postalInputs = document.querySelectorAll(".postalInput");
-    const measureInputs = document.querySelectorAll(".measureInput");
+const postalInputs = document.querySelectorAll(".postalInput");
+const measureInputs = document.querySelectorAll(".measureInput");
 
-    const dataTexts = document.querySelectorAll(".dataText");
+const dataTexts = document.querySelectorAll(".dataText");
 
-    var currentStep = 0;
-    const maxStep = steps.length;
+var currentStep = 0;
+const maxStep = steps.length;
 
-    var data = {
-        postalCode1: 0,
-        postalCode2: 0,
-        
-        lengthCm:0,
-        widthCm:0,
-        heightCm:0,
-        
-        weightKg: 0,
-
-        distance: 0,
-
-        gasPrice: 25,
-        weigthFee: 5,
-        fixedFee: 100,
-
-        finalPrice: 0,
-    };
-
-    backStepButton.addEventListener("click", ()=>{
-        if(currentStep > 0){
-            currentStep--
-            updateForm();
-        }
-    })
+var data = {
+    postalCode1: 0,
+    postalCode2: 0,
     
-    nextStepButton.addEventListener("click", () => {
-        currentStep++
-        console.log(currentStep)
-        switch(currentStep){
-            case 1:
-                if(postalInputs[0].value && postalInputs[1].value){
-                    updatePostalCodes();
-                }else{
-                    console.log("Postal codes not registered")
-                    alert("Postal codes not registered")
-                    currentStep--
-                }
-                break
-            case 2:
-                if(measureInputs[0].value && measureInputs[1].value && measureInputs[2].value){
-                    updateMeasures();
-                }else{
-                    console.log("Measures not registered")
-                    alert("Measures not registered")
-                    currentStep--
-                }
-                break
-        }
-        if(currentStep == 0){
-            backStepButton.classList.add("d-none")
-        }
-        else if(currentStep == maxStep - 1){
-            nextStepButton.textContent = "Calcular"
-            updateSummaryData();
-        }
-        else{
+    lengthCm:0,
+    widthCm:0,
+    heightCm:0,
+    
+    weightKg: 0,
+
+    distance: 0,
+
+    gasPrice: 25,
+    weigthFee: 5,
+    fixedFee: 100,
+
+    finalPrice: 0,
+};
+
+backStepButton.addEventListener("click", ()=>{
+    if(currentStep > 0){
+        currentStep--
+        updateForm();
+    }
+    if(currentStep == 0){
+        backStepButton.classList.add("d-none")
+    }
+})
+
+nextStepButton.addEventListener("click", () => {
+    currentStep++
+    console.log(currentStep)
+    switch(currentStep){
+        case 0:
+            backStepButton.classList.add("d-none");
+            break
+        case 1:
             backStepButton.classList.remove("d-none")
-        }
-    })
-    nextStepButton.addEventListener("click",()=>{
-        if(currentStep == maxStep){
+            if(postalInputs[0].value && postalInputs[1].value){
+                updatePostalCodes();
+            }else{
+                console.log("Postal codes not registered")
+                alert("Postal codes not registered")
+                currentStep--
+                backStepButton.classList.add("d-none");
+            }
+            break
+        case 2:
+            if(measureInputs[0].value && measureInputs[1].value && measureInputs[2].value){
+                updateMeasures();
+            }else{
+                console.log("Measures not registered")
+                alert("Measures not registered")
+                currentStep--
+                backStepButton.classList.add("d-none");
+            }
+            updateSummaryData();
+            break
+        case 3:
             priceCalc();
-        }
-    } )
-    // calculateButton.addEventListener("click", async() =>{
-    //     data.distance = await calculateDistance(data.postalCode1, data.postalCode2);
-    //     console.log(data.distance);
-    // })
+            nextStepButton.classList.add("d-none");
+            backStepButton.classList.add("d-none");
+            break
+    }
+    
+    
+})
 
-    function updateForm(){
-        
-        if(currentStep <= maxStep){
-            steps.forEach((step, index) => {
-                if(currentStep == (index)){
-                    step.classList.remove("d-none")
-                }else{
-                    step.classList.add("d-none")
-                }
-                
-            });
-            console.log("Form updated")
-            console.log(data);
-        }
-    }
-    function updatePostalCodes(){
-        data.postalCode1 = postalInputs[0].value
-        data.postalCode2 = postalInputs[1].value
-        updateForm();
-    }
-    function updateMeasures(){
-        data.lengthCm = measureInputs[0].value
-        data.widthCm = measureInputs[1].value
-        data.heightCm = measureInputs[2].value
-        data.weightKg = measureInputs[3].value
-        updateForm();
-    }
+function updateForm(){
     
-    function updateSummaryData(){
-        const values = Object.values(data);
-        console.log("Summary data updated")
-        console.log(dataTexts);
-        dataTexts.forEach((text, index)=>{
-            text.textContent = values[index] + text.textContent;
-        })
+    if(currentStep <= maxStep){
+        steps.forEach((step, index) => {
+            if(currentStep == (index)){
+                step.classList.remove("d-none")
+            }else{
+                step.classList.add("d-none")
+            }
+            
+        });
+        console.log("Form updated")
     }
-    async function priceCalc(){
-        data.distance = await calculateDistance(data.postalCode1, data.postalCode2);
-        var volumetricWeigth = Math.ceil((data.lengthCm*data.widthCm*data.heightCm)/5000);
-    
-        var gasPriceCalc = ((data.distance * data.gasPrice) /8);
-    
-    
-        if(volumetricWeigth >= data.weightKg){
-            data.weigthjKg = volumetricWeigth;
-        }
-        var weigthPrice = data.weightKg * data.weigthFee;
-        console.log(gasPriceCalc, weigthPrice, data.fixedFee)
-        console.log(data)
-        data.finalPrice = parseFloat(gasPriceCalc + weigthPrice + data.fixedFee);
-    
+    if(currentStep == 2){
+        nextStepButton.textContent = "Calcular"
     }
-})();
+    else{
+        nextStepButton.textContent = "Siguiente"
+    }
+}
+function updatePostalCodes(){
+    data.postalCode1 = postalInputs[0].value
+    data.postalCode2 = postalInputs[1].value
+    updateForm();
+}
+function updateMeasures(){
+    data.lengthCm = measureInputs[0].value
+    data.widthCm = measureInputs[1].value
+    data.heightCm = measureInputs[2].value
+    data.weightKg = measureInputs[3].value
+    updateForm();
+}
+
+function updateSummaryData(){
+    const values = Object.values(data);
+    console.log("Summary data updated")
+    console.log(dataTexts);
+    dataTexts.forEach((text, index)=>{
+        text.textContent = values[index] + text.textContent;
+    })
+}
+
+function updatePrice(){
+    const text = document.getElementById("priceText");
+    text.value = data.finalPrice + "$";
+    updateForm();
+}
+
+async function priceCalc(){
+    data.distance = await calculateDistance(data.postalCode1, data.postalCode2);
+    var volumetricWeigth = Math.ceil((data.lengthCm*data.widthCm*data.heightCm)/5000);
+
+    var gasPriceCalc = ((data.distance * data.gasPrice) /8);
+
+
+    if(volumetricWeigth >= data.weightKg){
+        data.weigthjKg = volumetricWeigth;
+    }
+    var weigthPrice = data.weightKg * data.weigthFee;
+    console.log(data)
+    data.finalPrice = parseFloat(gasPriceCalc + weigthPrice + data.fixedFee);
+    updatePrice();
+}
+
 
 
 
